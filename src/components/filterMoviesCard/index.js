5cf622d92891c2b166f4cca283fdc75520ddb003
Ruairi Byrne <ruairikbyrne@hotmail.com>
2021-06-08 19:16:16 +0100

    Added filtering of movie list

--- src/components/filterMoviesCard/index.js
+++ src/components/filterMoviesCard/index.js
@@ -55,6 +55,7 @@ export default function FilterMoviesCard(props) {
 
   const handleChange = (e, type, value) => {
     e.preventDefault()
+    props.onUserInput(type, value)   // NEW
     // Completed later
   };
   const handleTextChange = e => {
