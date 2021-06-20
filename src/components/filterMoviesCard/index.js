5c5553b8fa904c90ef77c7e6ab3e99392b3d9e00
Ruairi Byrne <ruairikbyrne@hotmail.com>
2021-06-20 14:43:08 +0100

    Using react-query to manage browser-based cache

--- src/components/filterMoviesCard/index.js
+++ src/components/filterMoviesCard/index.js
@@ -1,5 +1,6 @@
-//import React from "react";
-import React, {useState, useEffect}  from "react";
+import React from "react";
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -30,31 +31,29 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FilterMoviesCard(props) {
   const classes = useStyles();
-  
-  const [genres, setGenres] = useState([{ id: '0', name: "All" }])
-  //const genres = [
-  //  {id: 1, name: "Animation"},
-  //  {id: 2, name: "Comedy"},
-  //  {id: 3, name: "Thriller"}
-  //]
+  const { data, error, isLoading, isError } = useQuery("genres", getGenres);
 
-  useEffect(() => {
-    getGenres().then((allGenres) => {
-      setGenres([genres[0], ...allGenres]);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  const genres = data.genres;
+  genres.unshift({ id: "0", name: "All" });
 
   const handleChange = (e, type, value) => {
-    e.preventDefault()
-    props.onUserInput(type, value)   // NEW
-    // Completed later
+    e.preventDefault();
+    props.onUserInput(type, value); // NEW
   };
-  const handleTextChange = e => {
-    handleChange(e, "name", e.target.value)
-  }
-  const handleGenreChange = e => {
-    handleChange(e, "genre", e.target.value)
+
+  const handleTextChange = (e, props) => {
+    handleChange(e, "name", e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    handleChange(e, "genre", e.target.value);
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -6,21 +6,36 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <SiteHeader />      {/* New Header  */}
-      <Switch>
-        <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
-        <Route path="/reviews/:id" component={MovieReviewPage} />
-        <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-        <Route path="/movies/:id" component={MoviePage} />
-        <Route exact path="/" component={HomePage} />
-        <Redirect from="*" to="/" />
-      </Switch>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <SiteHeader />      {/* New Header  */}
+        <Switch>
+          <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
+          <Route path="/reviews/:id" component={MovieReviewPage} />
+          <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+          <Route path="/movies/:id" component={MoviePage} />
+          <Route exact path="/" component={HomePage} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
