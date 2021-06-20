681d1492c34d44893b1c99fd9ea584fb1004d841
Ruairi Byrne <ruairikbyrne@hotmail.com>
2021-06-20 16:47:32 +0100

    Fix upcoming movies page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -56,10 +56,12 @@
       });
   };
 
-  export const getUpcomingMovies = () => {
-    return fetch(
+  export const getUpcomingMovies = async() => {
+    const response = await fetch(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
     )
-      .then(res => res.json())
-      .then(json => json.results);
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
   };
--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,31 +1,34 @@
-import React, { useState, useEffect } from "react";
-import PageTemplate from '../components/templateMovieListPage'
+import React from "react";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from "react-query";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import Spinner from '../components/spinner';
+
 
 const UpcomingMoviesPage = (props) => {
-  const [movies, setMovies] = useState([]);
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
-
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
-
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   
   return (
     <PageTemplate
       title='Upcoming Movies'
       movies={movies}
-      selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}    
     />
   );
 };
